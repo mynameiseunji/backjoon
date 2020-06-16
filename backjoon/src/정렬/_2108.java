460d5fcbba845d2f411f57aa809c80adf7eeca04
mynameiseunji <create6v6@naver.com>
2020-06-16 15:17:09 +0900

    통계학 문제 풀이 완료

--- backjoon/src/정렬/_2108.java
+++ backjoon/src/정렬/_2108.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 /*
 첫째 줄에 수의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 그 다음 N개의 줄에는 정수들이 주어진다.
@@ -26,8 +27,10 @@ public class _2108 {
 			list.add(Integer.parseInt(br.readLine()));
 		}
 		Collections.sort(list);
-		avg(list);
-
+		System.out.println(Math.round(avg(list)));
+		System.out.println(center(list));
+		System.out.println(frequent(list));
+		System.out.println(range(list));
 	}
 	
 	//1. 산술평균 : N개의 수들의 합을 N으로 나눈 값
@@ -37,7 +40,7 @@ public class _2108 {
 			sum += list.get(i);
 		}
 		double avg = (double) sum / list.size();
-		return Math.round(avg);
+		return avg;
 	}
 	
 	//2. 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
@@ -48,28 +51,39 @@ public class _2108 {
 	//3. 최빈값 : N개의 수들 중 가장 많이 나타나는 값
 	private static int frequent(List<Integer> list) {
 		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int max = 0;
+		int index = 0;
+		List<Integer> key = new ArrayList<Integer>();
+		//key 값 : 수 , value 값 : 수의 count 갯수
 		for (int i = 0; i < list.size(); i++) {
 			if(map.containsKey(list.get(i))) {
 				map.replace(list.get(i), map.get(list.get(i))+1);
 			}else {
 				map.put(list.get(i), 1);
 			}
-		}
-		int max = 0;
-		int index = 0;
-		for(Integer i : map.keySet()) {
-			if(max<map.get(i)) {
-				max = map.get(i);
-				index = i;
+			//count가 가장 큰 수 찾기
+			if(max<=map.get(list.get(i))) {
+				max = map.get(list.get(i));
 			}
-		}            
-
+		}
+		for(Entry<Integer, Integer> m : map.entrySet()) {
+			if(m.getValue()==max) {
+				key.add(m.getKey());
+			} //최빈수인 key만 리스트에 저장
+		}
+		//최빈수가 여러개일 경우를 대비하여 오름차순 정렬
+		Collections.sort(key);
+		if(key.size()>1) { //최빈수가 여러개일 경우 2번째로 작은 수 출력
+			index = key.get(1);
+		}else { //최빈수가 하나일 경우
+			index = key.get(0);
+		}
 		return index;
 	}
 	
 	//4. 범위 : N개의 수들 중 최댓값과 최솟값의 차이
 	private static int range(List<Integer> list) {
-		return 0;
+		return list.get(list.size()-1)-list.get(0);
 	}
 
 }

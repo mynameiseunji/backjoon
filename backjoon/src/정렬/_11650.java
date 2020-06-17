17f157d1c9a7ed7e16e529dd7575f27785d9ceff
mynameiseunji <create6v6@naver.com>
2020-06-17 18:06:48 +0900

    시간초과나서 다시 푼 문제,,, comparator 익명함수 재정의로 해결!

--- backjoon/src/정렬/_11650.java
+++ backjoon/src/정렬/_11650.java
@@ -2,6 +2,8 @@ package 정렬;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class _11650 {
 
@@ -14,34 +16,47 @@ public class _11650 {
 			arr[i][0] = Integer.parseInt(s[0]);
 			arr[i][1] = Integer.parseInt(s[1]);
 		}
-		myXYSort(arr);
+		//comparator 익명함수 재정의로 시간초과 해결!
+		Arrays.sort(arr, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[0] == o2[0]) { //x좌표가 같을 경우 y좌표 비교 오름차순
+					return Integer.compare(o1[1], o2[1]);
+				}
+				return Integer.compare(o1[0], o2[0]); //x좌표 오름차순
+			}
+		});
+
+//		myXYSort(arr);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i][0] + " " + arr[i][1]);
 		}
 	}
 
-	public static void myXYSort(int[][] arr) {
-		int x = 0, y = 0;
-		for (int j = 0; j < arr.length; j++) {
-			for (int i = 0; i < arr.length - j - 1; i++) {
-				if (arr[i][0] > arr[i + 1][0]) {
-					x = arr[i][0];
-					y = arr[i][1];
-					arr[i][0] = arr[i + 1][0];
-					arr[i][1] = arr[i + 1][1];
-					arr[i + 1][0] = x;
-					arr[i + 1][1] = y;
-				} else if (arr[i][0] == arr[i + 1][0]) {
-					if (arr[i][1] > arr[i + 1][1]) {
-						x = arr[i][0];
-						y = arr[i][1];
-						arr[i][0] = arr[i + 1][0];
-						arr[i][1] = arr[i + 1][1];
-						arr[i + 1][0] = x;
-						arr[i + 1][1] = y;
-					}
-				}
-			}
-		}
-	}
+	// 버블정렬,,,시간초과!
+//	public static void myXYSort(int[][] arr) {
+//		int x = 0, y = 0;
+//		for (int j = 0; j < arr.length; j++) {
+//			for (int i = 0; i < arr.length - j - 1; i++) {
+//				if (arr[i][0] > arr[i + 1][0]) {
+//					x = arr[i][0];
+//					y = arr[i][1];
+//					arr[i][0] = arr[i + 1][0];
+//					arr[i][1] = arr[i + 1][1];
+//					arr[i + 1][0] = x;
+//					arr[i + 1][1] = y;
+//				} else if (arr[i][0] == arr[i + 1][0]) {
+//					if (arr[i][1] > arr[i + 1][1]) {
+//						x = arr[i][0];
+//						y = arr[i][1];
+//						arr[i][0] = arr[i + 1][0];
+//						arr[i][1] = arr[i + 1][1];
+//						arr[i + 1][0] = x;
+//						arr[i + 1][1] = y;
+//					}
+//				}
+//			}
+//		}
+//	}
 }

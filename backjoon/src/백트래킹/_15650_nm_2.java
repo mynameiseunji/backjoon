9a64477a56dc633544d340154ddabb89d2ab4b1b
mynameiseunji <create6v6@naver.com>
2020-08-26 14:52:56 +0900

    수정. 채점 성공!

--- backjoon/src/백트래킹/_15650_nm_2.java
+++ backjoon/src/백트래킹/_15650_nm_2.java
@@ -16,34 +16,35 @@ public class _15650_nm_2 {
 		String[] str = br.readLine().split(" ");
 		N = Integer.parseInt(str[0]);
 		M = Integer.parseInt(str[1]);
-		arr = new int[N + 1]; //N개의 자연수 배열
-		arr2 = new int[M]; //출력 순서를 저장하는 배열
-		for(int i=1;i<N+1;i++) {//초기화
-			arr[i]=i;
-		} 
-		visited = new int[N + 1]; //방문 표시 배열
-		pick(0);
-		
+		arr = new int[N + 1]; // N개의 자연수 배열
+		arr2 = new int[M + 1]; // 출력 순서를 저장하는 배열
+		for (int i = 1; i < N + 1; i++) {// 초기화
+			arr[i] = i;
+		}
+		visited = new int[N + 1]; // 방문 표시 배열
+		pick(1);
+
 	}
 
 	public static void pick(int cnt) {
-		if (cnt == M) { // 1. 종료 조건 : M개 달성시 출력
-			for (int i = 0; i < cnt; i++) {
-				System.out.print(arr2[i]+" ");
+		if (cnt == M + 1) { // 1. 종료 조건 : M개 달성시 출력
+			for (int i = 1; i < cnt; i++) {
+				System.out.print(arr2[i] + " ");
 			}
 			System.out.println();
 			return;
 		}
 		// 2. 그 외
 		for (int i = 1; i < N + 1; i++) {
-			if(cnt==0) { //재귀 첫 단계에서 방문 표시
-				visited[i-1]=1;
-			}
 			if (visited[i] == 0) {
-				visited[i]=1;
+				visited[i] = 1;
 				arr2[cnt] = arr[i];
-				pick(cnt+1);
-				visited[i]=0;
+				if (arr2[cnt] < arr2[cnt - 1]) {//오름차순 확인 추가
+					visited[i] = 0;
+					continue;
+				}
+				pick(cnt + 1);
+				visited[i] = 0;
 			}
 		}
 	}

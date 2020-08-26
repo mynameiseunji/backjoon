dff6246ac0efd70d7ade1765333c8575350c5526
mynameiseunji <create6v6@naver.com>
2020-08-26 12:06:32 +0900

    드디어 풀었당 재귀 구조는 그리면서 풀어보자~

---
+++ backjoon/src/백트래킹/_15649_nm_1.java
@@ -0,0 +1,48 @@
+package 백트래킹;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _15649_nm_1 {
+
+	private static int N;
+	private static int M;
+	private static int[] arr;
+	private static int[] arr2;
+	private static int[] visited;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
+		arr = new int[N + 1]; //N개의 자연수 배열
+		arr2 = new int[M]; //출력 순서를 저장하는 배열
+		for(int i=1;i<N+1;i++) {//초기화
+			arr[i]=i;
+		} 
+		visited = new int[N + 1]; //방문 표시 배열
+		pick(0);
+		
+	}
+
+	public static void pick(int cnt) {
+		if (cnt == M) { // 1. 종료 조건 : M개 달성시 출력
+			for (int i = 0; i < cnt; i++) {
+				System.out.print(arr2[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		// 2. 그 외
+		for (int i = 1; i < N + 1; i++) {
+			if (visited[i] == 0) {
+				visited[i]=1;
+				arr2[cnt] = arr[i];
+				pick(cnt+1);
+				visited[i]=0;
+			}
+		}
+	}
+
+}
